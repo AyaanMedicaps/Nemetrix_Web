ec1a2f132750c57e8b73c40f7212dc08be03cda8
AyaanMedicaps <ayanchoudhary1005@gmail.com>
2025-03-22 22:33:18 +0530

    building web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "369637779"
+    serviceWorkerVersion: "2873271139"
   }
 });
